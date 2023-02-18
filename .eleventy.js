47a189fd335fadc2fd537a7d71230d39407b5ed4
Micah Torcellini <micah@torcellini.org>
2023-02-18 08:47:11 -0500

    layouts

---
+++ .eleventy.js
@@ -0,0 +1,143 @@
+const { DateTime } = require("luxon");
+const fs = require("fs");
+const pluginRss = require("@11ty/eleventy-plugin-rss");
+const slugify = require("slugify");
+const striptags = require("striptags");
+
+module.exports = function(eleventyConfig) {
+  eleventyConfig.addPlugin(pluginRss);
+  eleventyConfig.setDataDeepMerge(true);
+
+  eleventyConfig.addLayoutAlias("base", "layouts/base.njk");
+  eleventyConfig.addLayoutAlias("article", "layouts/article.njk");
+
+  eleventyConfig.addFilter("readableDate", dateObj => {
+    return DateTime.fromJSDate(dateObj, {zone: 'utc'}).toFormat("LLLL dd, yyyy");
+  });
+  eleventyConfig.addFilter("slugify-removeChars", function(value) {
+    let product = slugify(value, {
+      replacement: '-',    // replace spaces with replacement
+      remove: /[*+~.()'"!:@’“”]/g,    // regex to remove characters
+      lower: true          // result in lower case
+    })
+    return product;
+  });
+  eleventyConfig.addFilter("striptags", function(value) {
+      let product = striptags(value);
+      return product;
+  });
+
+  // shortcodes 
+  eleventyConfig.addShortcode("ytEmbed", require("./_11ty/ytEmbed.js"));
+
+  // https://html.spec.whatwg.org/multipage/common-microsyntaxes.html#valid-date-string
+  eleventyConfig.addFilter('htmlDateString', (dateObj) => {
+    return DateTime.fromJSDate(dateObj, {zone: 'utc'}).toFormat('yyyy-LL-dd');
+  });
+
+  // Get the first `n` elements of a collection.
+  eleventyConfig.addFilter("head", (array, n) => {
+    if( n < 0 ) {
+      return array.slice(n);
+    }
+
+    return array.slice(0, n);
+  });
+
+  eleventyConfig.addCollection("tagList", require("./_11ty/getTagList"));
+// passthrough
+  eleventyConfig.addPassthroughCopy("assets/js");
+  eleventyConfig.addPassthroughCopy("assets/css");
+  eleventyConfig.addPassthroughCopy("assets/files");
+  eleventyConfig.addPassthroughCopy("assets/fonts");
+  eleventyConfig.addPassthroughCopy("assets/images/site");
+  eleventyConfig.addPassthroughCopy("robots.txt");
+  eleventyConfig.addPassthroughCopy("CNAME");
+  eleventyConfig.addPassthroughCopy("favicon.ico");
+
+  /* Markdown Plugins */
+  let markdownIt = require("markdown-it");
+  let markdownItAnchor = require("markdown-it-anchor");
+  let markdownItDeflist = require("markdown-it-deflist");
+  let markdownItFootnote = require('markdown-it-footnote');
+  let markdownItAttrs = require("markdown-it-attrs");
+  let mdOptions = {
+    html: true,
+    breaks: false,
+    linkify: true,
+    typographer: true
+  };
+  let anchorOptions = {
+    permalink: false,
+    permalinkClass: "direct-link",
+    permalinkSymbol: "#"
+  };
+
+  let md = markdownIt(mdOptions)
+    .use(markdownItAnchor, anchorOptions)
+    .use(markdownItDeflist)
+    .use(markdownItAttrs)
+    .use(markdownItFootnote);
+
+  eleventyConfig.setLibrary("md", md);
+
+  md.renderer.rules.footnote_block_open = function () {
+      return '<h2>Footnotes</h2> \n <ol>';
+  };
+  md.renderer.rules.footnote_block_close = function () {
+    return '</ol>';
+  };
+  md.renderer.rules.footnote_anchor = function () {
+    return "";
+  };
+
+  md.renderer.rules.footnote_caption = function (tokens, idx) {
+    var n = Number(tokens[idx].meta.id + 1).toString();
+
+    if (tokens[idx].meta.subId > 0) {
+      n += ':' + tokens[idx].meta.subId;
+    }
+  
+    return  n;
+  };
+
+  eleventyConfig.setBrowserSyncConfig({
+    callbacks: {
+      ready: function(err, browserSync) {
+        const content_404 = fs.readFileSync('_site/404.html');
+
+        browserSync.addMiddleware("*", (req, res) => {
+          // Provides the 404 content without redirect.
+          res.write(content_404);
+          res.end();
+        });
+      }
+    }
+  });
+
+  return {
+    templateFormats: [
+      "md",
+      "njk",
+      "html",
+      "liquid"
+    ],
+
+    // If your site lives in a different subdirectory, change this.
+    // Leading or trailing slashes are all normalized away, so don’t worry about it.
+    // If you don’t have a subdirectory, use "" or "/" (they do the same thing)
+    // This is only used for URLs (it does not affect your file structure)
+    pathPrefix: "/",
+
+    markdownTemplateEngine: "liquid",
+    htmlTemplateEngine: "njk",
+    dataTemplateEngine: "njk",
+    passthroughFileCopy: true,
+    dir: {
+      input: ".",
+      includes: "_includes",
+      data: "_data",
+      output: "_site"
+    }
+  }
+};
