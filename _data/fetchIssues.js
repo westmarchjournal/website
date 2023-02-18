430ce6391a266ef5e0929eed9b514a0fc53e71c8
Micah Torcellini <micah@torcellini.org>
2023-02-18 18:41:51 -0500

    fetchIssues.js

---
+++ _data/fetchIssues.js
@@ -0,0 +1,62 @@
+// it looks like a point-and-click video game--Oh, it's Westmarch, which is basically a point-and-click video game.--Seth Nowak
+const fs = require("fs");
+const path = require("path")
+// returns issue data in an array
+// returns an array of subdirectories in a directory, see https://stackoverflow.com/questions/18112204/get-all-directories-within-directory-nodejs
+function getSubdirs (dir) {
+    return fs.readdirSync(dir, { withFileTypes: true})
+        .filter(dirent => dirent.isDirectory())
+        .map(dirent => dirent.name);
+}
+
+// returns the first JSON file as an object
+// Potential problem: if there is more than one JSON file, this will throw an error
+// TODO: fix this by matching the filename
+function getJSON (filePath, dirname) {
+    // gets a list of items
+    items = fs.readdirSync(filePath, { withFileTypes: true })
+        .filter(function(item) {
+            return path.extname(filePath + "/" + item.name) == ".json"
+        })
+    // checks that we don't have more than one JSON file, viz. more than one item in the array
+    if (items[1]) {
+        throw error("More than one JSON file in" + filePath)
+    }
+    // selects the first item in the array
+    f = items[0]
+    // returns the parsed JSON object
+    return JSON.parse(
+        fs.readFileSync(filePath + "/" + f.name)
+    )
+}
+
+// returns a list of all sub-sub-directories
+function getDirList(dir) {
+    let dircontent = getSubdirs(dir);
+    let dirlist = [];
+    // takes `dircontent` and return an array of all subdirectories
+    dircontent.forEach(function(subdir) {
+        let filePath = dir + "/" + subdir
+        subdirs = getSubdirs(filePath)
+        // adds each sub-sub directory to the `dirlist` array
+        subdirs.forEach(function(subdir) {
+            dirlist.push(filePath + "/" + subdir)
+        })
+    })
+    return dirlist;
+};
+
+// the main function
+function issues(dir) {
+    let dirs = getDirList(dir)
+    issues = [];
+    dirs.forEach(function(filePath) {
+        json = getJSON(filePath)
+        issues.push(json)
+    })
+    return issues
+}
+
+module.exports = function ( dir = "articles") {
+    return issues(dir)
+}
